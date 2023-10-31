457cdbedad9491e0d746a1c9b49d919ae828840a
cdkirby276 <139935601+cdkirby276@users.noreply.github.com>
2023-10-31 16:50:42 -0500

    Create Inventory.py
    Created Base file for Inventory Class

---
+++ Inventory.py
@@ -0,0 +1,12 @@
+class Inventory:
+    def __init__(self, databaseName, tableName):
+        self.databaseName = databaseName
+        self.tableName = tableName
+    def viewInventory():
+        pass
+    def searchInventory():
+        pass
+    def decreaseStock(ISBN):
+        pass
+
+
