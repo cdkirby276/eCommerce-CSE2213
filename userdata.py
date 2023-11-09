957e5ce06b13c47e2fc8487bbc34ca871026f4c3
btsheck <149538046+btsheck@users.noreply.github.com>
2023-11-09 14:57:20 -0600

    Add files via upload

---
+++ userdata.py
@@ -0,0 +1,20 @@
+class User:
+    def __init__(self, databaseName, tableName):
+        self.databaseName = databaseName
+        self.tableName = tableName
+    def Get_User():
+        pass
+    def Get_Password():
+        pass
+    def is_admin():
+        pass
+    def Get_address():
+        pass
+    def Set_User():
+        pass
+    def Set_Password():
+        pass
+    def Set_Admin():
+        pass
+    def Set_Address():
+        pass
