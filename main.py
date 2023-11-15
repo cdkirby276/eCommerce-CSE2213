6fe2d8ed385c1d4ff07122570f56318678c7b7b1
jscruppi <132865800+jscruppi@users.noreply.github.com>
2023-11-15 16:12:49 -0600

    Worked on main
    Worked on the menu options where user is looking inside the cart, majority of the options are now completed
    
    Also finished parts of the pre-login state that I forgot about in the first upload

--- main.py
+++ main.py
@@ -2,26 +2,20 @@
 main class for group project
 
 """
-
+import sqlite3
+from sys import exit
 from Inventory import Inventory
 from Cart import Cart
 from userdata import User
 
 def main():
     """
-
-        Before Login state:
-        User will be able to:
-            Login
-            Create Account
-            Logout
-        
+    Before Login state:
+    User will be able to:
+        -Login           
+        -Create Account
+        -Logout
     """
-
-    #prompt user to start using the program
-    print('Please input the correct command in order to do the following:\n(1)Login\n(2)Logout\n(3)Create Account')
-    menuOption = input()
-
     #will create a user and pass it the db and table name for users
     #currernt_user = User('user.db','User')
     current_user = User()
@@ -30,18 +24,23 @@ def main():
 
     #loop the prelogin state until someone logs in
     while(not current_user.getLoggedIn()):
+        #prompt user to start using the program
+        print('Please input the correct command in order to do the following:\n(1)Login\n(2)Logout\n(3)Create Account')
+        menuOption = input()
+
         match(menuOption):
             case '1':
                 print('Logging in')
-                #current_user.login()
+                current_user.login()
                 mainMenu(current_user)
             case '2':
                 print('Logging out')
-                #current_user.logout()
+                current_user.logout()
             case '3':
                 print('Creating account')
+                current_user.createAccount()
             case _:
-                print('Invalid menu option, please select again')
+                print('Invalid menu option, please select again\n')
 
 
 """
@@ -74,12 +73,9 @@ def mainMenu(current_user):
                 temp_cart.viewCart(current_user.getUserID(), 'Inventory.db') #will view the user's "cart"
                 break
             case _:
-                print('Invalid menu option, please select again')
+                print('Invalid menu option, please select again\n')
     
-        
-
     #return to main once user logs out
-    return 
 
 """
 This is where we go when the use wants to view his/her cart
@@ -92,8 +88,40 @@ User will be able to:
 
 """
 def viewingCart(current_user):
+
+    #make a temp cart
+    temp_cart = Cart('cart.db','cart')
+    while(True):
+        #prompt user
+        print('Please input the correct command in order to do the following:')
+        print('(1)Return\n(2)View Cart Items\n(3)Add to cart\n(4)Remove from cart\n(5)Checkout')
+        menu_option = input()
+
+        match(menu_option):
+            case '1':
+                break #will break out of the loop and also return back to main menu
+            case '2':
+                temp_cart.viewCart('Inventory.db') #displays what is in the cart
+            case '3':
+                #FINISH::
+                #will add something to the cart, need to decide how we will do that
+                #how to display to user what they can add
+                #can either display whole inventory or have them search for a book
+                pass
+            case '4':
+                #FINISH::
+                #need to use databases (maybe) to find the ISBN that we need to remove
+                print('What book would you like to remove?')
+                temp_book = input()
+                temp_cart.removeFromCart()
+            case '5':
+                print('Checking out now')
+                temp_cart.checkOut(current_user.getUserID()) #calls the checkout function and then breaks to main menu
+                break
+            case _:
+                print('Invalid menu option, please select again:\n')
+    
     #returns to main menu
-    return 
 
 
 #when script is ran
