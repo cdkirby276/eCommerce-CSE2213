65fdf4f82ad72874dbf69a164f9b201c7f4b16bd
jscruppi <132865800+jscruppi@users.noreply.github.com>
2023-11-28 11:09:29 -0600

    Finalized Cart SQL
    Cart database now uses varchar ISBN numbers to match Inventory's ISBN, and Cart table now matches the current Inventory database

--- cart.sql
+++ cart.sql
@@ -1,5 +1,20 @@
 CREATE TABLE 'cart' (
     'UserID' varchar(14) NOT NULL,
-    'ISBN' int(9) NOT NULL,
+    'ISBN' varchar(13) NOT NULL,
     'Quantity' int(2) DEFAULT NULL
 );
+
+INSERT INTO cart (UserID, ISBN, Quantity)
+VALUES ('999', '9780061120084', 1);
+
+INSERT INTO cart (UserID, ISBN, Quantity)
+VALUES ('999', '9781400079020', 1);
+
+INSERT INTO cart (UserID, ISBN, Quantity)
+VALUES ('999', '9780743273565', 1);
+
+INSERT INTO cart (UserID, ISBN, Quantity)
+VALUES ('775', '9780547928227', 1);
+
+INSERT INTO cart (UserID, ISBN, Quantity)
+VALUES ('775', '9780140449334', 1); 
